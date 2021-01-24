33a9409f84e935ebd51d33fd0fe411644761a922
Danil Kolumbet <kolumbetko@gmail.com>
2021-01-24 06:05:37 +0200

    Add input parsing

--- visualbasm.js
+++ visualbasm.js
@@ -3,5 +3,15 @@ const canvas = document.querySelector("#Canvas");
 const ctx = canvas.getContext("2d", {alpha: false});
 let width, height;
 
-ctx.fillStyle = 'blue';
+ctx.fillStyle = "blue";
 ctx.fillRect(10, 10, 40, 40);
+
+const inputElem = document.querySelector("#InputElem");
+
+let inputProgram = inputElem.value
+    .split("\n")
+    .map(line => line.trim())
+    .filter(String)
+    .filter(line => !line.startsWith("%"));
+
+console.table(inputProgram);
